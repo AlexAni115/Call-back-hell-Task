afd15c6b64d88ce0d43e1a5cea6ceeb8bfbdcedb
AlexAni115 <lalex115@gmail.com>
2023-11-27 11:45:05 +0530

    Completed

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+    
+    document.getElementById("Callback").innerText="10"
+    setTimeout(() => {
+        document.getElementById("Callback").innerText="9"
+        setTimeout(() => {
+            document.getElementById("Callback").innerText="8"
+            setTimeout(() => {
+                document.getElementById("Callback").innerText="7"
+                setTimeout(() => {
+                    document.getElementById("Callback").innerText="6"
+                    setTimeout(() => {
+                        document.getElementById("Callback").innerText="5"
+                        setTimeout(() => {
+                            document.getElementById("Callback").innerText="4"
+                            setTimeout(() => {
+                                document.getElementById("Callback").innerText="3"
+                                setTimeout(() => {
+                                    document.getElementById("Callback").innerText="2"
+                                    setTimeout(() => {
+                                        document.getElementById("Callback").innerText="1"
+                                        setTimeout(() => {
+                                            document.getElementById("Callback").innerText="Happy Independence Day..!!"
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
